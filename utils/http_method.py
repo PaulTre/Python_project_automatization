9bedecd6845ca49ab8978ce8d58c7575344e7b89
PaulTre <Gebankpartizanskiy@gmail.com>
2024-09-19 12:22:07 +0300

    add new class HTTP methods

--- utils/http_method.py
+++ utils/http_method.py
@@ -1,1 +1,27 @@
-print("Hello world!")
+import requests
+
+"""Список HTTP методов"""
+
+class http_methods:
+    headers = {"Content-Type: 'application/json"}
+    cookies = ""
+
+    @staticmethod
+    def get(url):
+        result = requests.get(url, headers = http_methods.headers, cookies= http_methods.cookies)
+        return result
+
+    @staticmethod
+    def post(url, body):
+        result = requests.post(url, json=body, headers=http_methods.headers, cookies=http_methods.cookies)
+        return result
+
+    @staticmethod
+    def put(url, body):
+        result = requests.put(url, json=body, headers=http_methods.headers, cookies=http_methods.cookies)
+        return result
+
+    @staticmethod
+    def delete(url, body):
+        result = requests.delete(url, json=body, headers=http_methods.headers, cookies=http_methods.cookies)
+        return result
